8f6e220293089ef7e120989497cdce2a7b2c6c56
Fernando Zanei <fernandozanei@icloud.com>
2018-03-18 10:34:42 -0400

    layout static

--- Tap-It-Layout/ViewController.swift
+++ Tap-It-Layout/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
+    
+    var layouted = false
+    let bottomCard = UIImageView()
+    let topCard = UIImageView()
+    let stackView = UIStackView()
+
     var top1: PlayerScore!
     var top2: PlayerScore!
     var top3: PlayerScore!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,73 +31,74 @@ class ViewController: UIViewController {
         gradientLayer.frame = view.frame
         view.layer.insertSublayer(gradientLayer, at: 0)
         
-        // BOTTOM CARD
-        
-        let bottomCard = UIImageView()
-        bottomCard.image = UIImage(named: "card_3")
-        bottomCard.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bottomCard)
-        
-        bottomCard.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 8).isActive = true
-        bottomCard.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -8).isActive = true
-        bottomCard.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
-        bottomCard.widthAnchor.constraint(equalTo: bottomCard.heightAnchor, constant: 0).isActive = true
-        bottomCard.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-
-        // BOTTOM CARD
-        
-        let topCard = UIImageView()
-        topCard.image = UIImage(named: "card_3")
-        topCard.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(topCard)
-        topCard.centerXAnchor.constraint(equalTo: bottomCard.centerXAnchor, constant: 0).isActive = true
-        topCard.widthAnchor.constraint(equalTo: bottomCard.widthAnchor, multiplier: 0.8).isActive = true
-        topCard.heightAnchor.constraint(equalTo: bottomCard.heightAnchor, multiplier: 0.8).isActive = true
-        topCard.bottomAnchor.constraint(equalTo: bottomCard.topAnchor, constant: -16).isActive = true
-        //topCard.
-        
-        if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
-            self.top1 = scoreview
-        }
-
-        if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
-            self.top2 = scoreview
-        }
-
-        if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
-            self.top3 = scoreview
-        }
-
-        let stackview = UIStackView()
-        stackview.axis = .horizontal
-        stackview.distribution = .equalSpacing
-//        stackview.spacing = 50.0
-        stackview.addArrangedSubview(top1)
-        stackview.addArrangedSubview(top2)
-        stackview.addArrangedSubview(top3)
-
-        stackview.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(stackview)
-
-        stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
-        stackview.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 32).isActive = true
-        stackview.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -32).isActive = true
-        stackview.bottomAnchor.constraint(equalTo: topCard.topAnchor, constant: -8).isActive = true
-        stackview.heightAnchor.constraint(equalTo: bottomCard.heightAnchor, multiplier: 0.20).isActive = true
-
-        // SCORE VIEW
-        
-        
-        
-        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        
+        if !layouted {
+            let frame = view.safeAreaLayoutGuide.layoutFrame
+            let safeTop = view.safeAreaInsets.top
+            
+            // BOTTOM CARD - PLAYER
+            
+            bottomCard.image = UIImage(named: "card_1")
+            
+            var tempHeight = CGFloat(0.0)
+            var tempWidth = CGFloat(0.0)
+            
+            tempHeight = frame.height * 0.47
+            tempWidth = tempHeight
+            
+            bottomCard.frame = CGRect(x: (frame.width/2)-(tempWidth/2), y: frame.height+safeTop-tempHeight-8, width: tempWidth, height: tempHeight)
+            
+            view.addSubview(bottomCard)
+
+            // TOP CARD - DECK
+            
+            topCard.image = UIImage(named: "card_3")
+            
+            tempHeight = frame.height * 0.37
+            tempWidth = tempHeight
+            
+            topCard.frame = CGRect(x: (frame.width/2)-(tempWidth/2), y: bottomCard.frame.minY-tempHeight-8, width: tempWidth, height: tempHeight)
+
+            view.addSubview(topCard)
+            
+            // STACK VIEW - SCORE
+            
+            stackView.axis = .horizontal
+            stackView.distribution = .equalSpacing
+            
+            tempHeight = topCard.frame.minY-safeTop-16
+            tempWidth = frame.width-32
+            
+            stackView.frame = CGRect(x: (frame.width/2)-(tempWidth/2), y: safeTop+8, width: tempWidth, height: tempHeight)
+            
+            view.addSubview(stackView)
+
+            
+            
+            if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
+                self.top1 = scoreview
+            }
+            
+            if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
+                self.top2 = scoreview
+            }
+            
+            if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
+                self.top3 = scoreview
+            }
+            
+            stackView.addArrangedSubview(top1)
+            stackView.addArrangedSubview(top2)
+            stackView.addArrangedSubview(top3)
+
+            
+            layouted = true
+        }
     }
-
-
+    
+    
 }
 
